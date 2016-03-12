40d713b6766567b162e19b13b411f39baeb4801a
wtsst <staryumou@163.com>
2016-03-12 23:49:21 +0800

    wohenkaixin我很开心提交成功

--- app/src/main/java/com/hp_hp/asuper/MainActivity.java
+++ app/src/main/java/com/hp_hp/asuper/MainActivity.java
@@ -3,10 +3,12 @@ package com.hp_hp.asuper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
+    private TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
