6f9b61fd1f3daf692ea23e6178e56fc045c2d95c
wtsst <staryumou@163.com>
2016-03-12 23:14:34 +0800

    sunshuntao

--- app/src/main/java/com/hp_hp/asuper/MainActivity.java
+++ app/src/main/java/com/hp_hp/asuper/MainActivity.java
@@ -2,12 +2,16 @@ package com.hp_hp.asuper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
     }
 }
