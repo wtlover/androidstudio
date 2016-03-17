35a9e4c6eb60b52cfd990dbbc354ab4157ad0aac
wtsst <staryumou@163.com>
2016-03-17 16:41:07 +0800

    sunshuntao

--- app/src/main/java/com/hp_hp/asuper/MainActivity.java
+++ app/src/main/java/com/hp_hp/asuper/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
     private TextView mTextView;
+    private TextView ss;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
